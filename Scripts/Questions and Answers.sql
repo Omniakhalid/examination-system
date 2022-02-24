0fcd3ce7bb440c401675a6404af32abaaecb8e3e
Omniakhalid <63504895+Omniakhalid@users.noreply.github.com>
2022-02-24 15:33:44 +0200

    add q&a

---
+++ Scripts/Questions and Answers.sql
@@ -0,0 +1,326 @@
+﻿---------------------SQLSERVER MCQ Question----------------------------
+insert_question 1, 'When was the first version of Microsoft SQL Server released?','mcq', '1988',  5,1
+go
+insert_question 2, '_________ is free database software running free SQL Server technology.', 'mcq',  'SQL Server Express', 5, 1
+go
+insert_question 3, 'Which of the following is not a database object?','mcq', 'PowerShell',  5, 1
+go
+insert_question 4, '______________ is schematic drawing used for representing relationships in database.', 'mcq', 'Database Diagrams',5, 1
+go
+insert_question 5, 'The _________ database is used by SQL Server Agent for scheduling alerts and jobs, and recording operators.', 'mcq', 'msdb', 5, 1
+go
+insert_question 6, 'Which of the following is a large object data type?', 'mcq', 'image', 5, 1
+go
+insert_question 7, 'Data types in SQL Server are organized into how many categories?', 'mcq', '6', 5, 1
+go
+insert_question 8, '______________ function returns current date and time.', 'mcq', 'GETDATE', 5, 1
+go
+insert_question 9, 'Which SQL keyword is used to retrieve a maximum value?', 'mcq', 'MAX',5, 1
+go
+insert_question 10, 'Which of the following is not a DDL command?', 'mcq', 'UPDATE', 5, 1
+go
+---------------------SQLSERVER T/F Question----------------------------
+insert_question 11, 'The condition in a WHERE clause can refer to only one value.','t/f',  'False', 3, 1
+go
+insert_question 12, 'The ADD command is used to enter one row of data or to add multiple rows as a result of a query.','t/f',  'False', 3, 1
+go 
+insert_question 13, 'A SELECT statement within another SELECT statement and enclosed in square brackets ([...]) is called a subquery.','t/f', 'False', 3, 1
+go
+insert_question 14, 'SQL is a programming language.','t/f', 'False', 3, 1
+go
+insert_question 15, 'SELECT DISTINCT is used if a user wishes to see duplicate columns in a query.','t/f', 'False', 3, 1
+go
+insert_question 16, 'Indexes can usually be created for both primary and secondary keys.','t/f', 'True', 3, 1
+go
+insert_question 17, 'The HAVING clause acts like a WHERE clause, but it identifies groups that meet a criterion, rather than rows.','t/f', 'True', 3, 1
+go
+insert_question 18, 'SQL is a data sublanguage.','t/f', 'True', 3, 1
+go
+insert_question 19,  'ORDER BY can be combined with the SELECT statements.','t/f', 'True', 3, 1
+go
+insert_question 20, 'Data manipulation language (DML) commands are used to define a database, including creating, altering, and dropping tables and establishing constraints.','t/f', 'False', 3, 1
+go
+---------------------C# MCQ Question----------------------------
+insert_question 21, 'The Method use to remove white space from a string?', 'mcq', 'Trim()', 5, 2 
+go
+insert_question 22, 'Among the given collections which one is I/O index based?', 'mcq', 'ArrayList', 5, 2 
+go
+insert_question 23, 'In which of the following collections is the I/O based on a key?', 'mcq', 'SortedList', 5, 2 
+go
+insert_question 24, 'Which of these is used to access members of class before the object of that class is created?', 'mcq', 'static', 5, 2 
+go
+insert_question 25, 'How many Bytes are stored by Long Data type in C# .net?', 'mcq', '8', 5, 2 
+go
+insert_question 26, 'Default Type of number without decimal is?', 'mcq', 'Int', 5, 2 
+go
+insert_question 27, 'The correct way of incrementing the operators are:', 'mcq', 'c += 1', 5, 2 
+go
+insert_question 28, 'Which of the following is/are not Relational operators in C#.NET?', 'mcq', '<>=', 5, 2 
+go
+insert_question 29, 'What is the process by which we can control parts of a program that can access the members of a class?', 'mcq', 'Encapsulation', 5, 2 
+go
+insert_question 30, 'Which of these operators can be used to concatenate two or more String objects?', 'mcq', '+', 5, 2 
+go
+---------------------C# T/F Question----------------------------
+insert_question 31, 'A function can return more than one value.','t/f', 'False',3,2
+go
+insert_question 32, 'If a function returns no value, the return type must be declared as void','t/f','True',3,2
+go
+insert_question 33, 'A local variable declared in a function is not usable out side that function.','t/f','True',3,2
+go
+insert_question 34, 'Arithmetic operators are called binary operators when you use two arguments with each operator.','t/f','True',3, 2
+go
+insert_question 35,  'The data components of a class often are called its iteration','t/f','False',3,2
+go
+insert_question 36, 'The public modifier allows unlimited access to a method.','t/f','True',3,2
+go
+insert_question 37, 'You declare a Boolean variable by using type integer.','t/f','False',3,2
+go
+insert_question 38, 'A parent class is a synonym for base class.','t/f','True',3,2
+go
+insert_question 39, 'The new operator is used to create objects.','t/f','True',3,2
+go
+insert_question 40, 'The conditional AND operator is written as &&','t/f','True',3,2
+go
+
+---------------------SQLSERVER CHOICES----------------------------
+insert_exam_choices 1, '1983'
+go
+insert_exam_choices 1, '1988'
+go
+insert_exam_choices 1, '1990'
+go
+insert_exam_choices 1, '1991'
+go
+insert_exam_choices 2, 'SQL Server Express'
+go
+insert_exam_choices 2, 'SQL Server Workgroup'
+go
+insert_exam_choices 2, 'SQL Server Enterprise'
+go
+insert_exam_choices 2, 'SQL Server Web'
+go
+insert_exam_choices 3, 'Database Diagrams'
+go
+insert_exam_choices 3, 'Stored Procedures'
+go
+insert_exam_choices 3, 'PowerShell'
+go
+insert_exam_choices 3, 'Index'
+go
+insert_exam_choices 4, 'Trigger'
+go
+insert_exam_choices 4, 'Database Diagrams'
+go
+insert_exam_choices 4, 'Table designer'
+go
+insert_exam_choices 4, 'Query Editor'
+go
+insert_exam_choices 5, 'model'
+go
+insert_exam_choices 5, 'msdb'
+go
+insert_exam_choices 5, 'master'
+go
+insert_exam_choices 5, 'temp'
+go
+insert_exam_choices 6, 'varchar(max)'
+go
+insert_exam_choices 6, 'varbinary(max)'
+go
+insert_exam_choices 6, 'nvarchar(max)'
+go
+insert_exam_choices 6, 'image'
+go
+insert_exam_choices 7, '6'
+go
+insert_exam_choices 7, '8'
+go
+insert_exam_choices 7, '9'
+go
+insert_exam_choices 7, '10'
+go
+insert_exam_choices 8, 'SET DATEFIRST'
+go
+insert_exam_choices 8, 'SYSDATETIME'
+go
+insert_exam_choices 8, 'Cert_ID'
+go
+insert_exam_choices 8, 'GETDATE'
+go
+insert_exam_choices 9, 'MOST'
+go
+insert_exam_choices 9, 'TOP'
+go
+insert_exam_choices 9, 'MAX'
+go
+insert_exam_choices 9, 'UPPER'
+go
+insert_exam_choices 10, 'UPDATE'
+go
+insert_exam_choices 10, 'TRUNCATE'
+go
+insert_exam_choices 10, 'ALTER'
+go
+insert_exam_choices 10, 'NONE'
+go
+insert_exam_choices 11, 'True'
+go
+insert_exam_choices 11, 'False'
+go
+insert_exam_choices 12, 'True'
+go
+insert_exam_choices 12, 'False'
+go
+insert_exam_choices 13, 'True'
+go
+insert_exam_choices 13, 'False'
+go
+insert_exam_choices 14, 'True'
+go
+insert_exam_choices 14, 'False'
+go
+insert_exam_choices 15, 'True'
+go
+insert_exam_choices 15, 'False'
+go
+insert_exam_choices 16, 'True'
+go
+insert_exam_choices 16, 'False'
+go
+insert_exam_choices 17, 'True'
+go
+insert_exam_choices 17, 'False'
+go
+insert_exam_choices 18, 'True'
+go
+insert_exam_choices 18, 'False'
+go
+insert_exam_choices 19, 'True'
+go
+insert_exam_choices 19, 'False'
+go
+insert_exam_choices 20, 'True'
+go
+insert_exam_choices 20, 'False'
+go
+---------------------C# CHOICES----------------------------
+insert_exam_choices 21, 'Split()'
+go
+insert_exam_choices 21, 'Substring()'
+go
+insert_exam_choices 21, 'Trim()'
+go
+insert_exam_choices 21, 'TrimStart()'
+go
+insert_exam_choices 22, 'ArrayList'
+go
+insert_exam_choices 22, 'List'
+go
+insert_exam_choices 22, 'Stack'
+go
+insert_exam_choices 22, 'Queue'
+go
+insert_exam_choices 23, 'BitArray'
+go
+insert_exam_choices 23, 'SortedList'
+go
+insert_exam_choices 23, 'Queue'
+go
+insert_exam_choices 23, 'Stack'
+go
+insert_exam_choices 24, 'public'
+go
+insert_exam_choices 24, 'private'
+go
+insert_exam_choices 24, 'static'
+go
+insert_exam_choices 24, 'protected'
+go
+insert_exam_choices 25, '8'
+go
+insert_exam_choices 25, '4'
+go
+insert_exam_choices 25, '2'
+go
+insert_exam_choices 25, '1'
+go
+insert_exam_choices 26,'Long Int'
+go
+insert_exam_choices 26,'Unsigned Long'
+go
+insert_exam_choices 26,'Int'
+go
+insert_exam_choices 26,'Unsigned Int'
+go
+insert_exam_choices 27, '++ a ++'
+go
+insert_exam_choices 27, 'b ++ 1'
+go
+insert_exam_choices 27, 'c += 1'
+go
+insert_exam_choices 27, 'd =+ 1'
+go
+insert_exam_choices 28, '>='
+go
+insert_exam_choices 28, '<>='
+go
+insert_exam_choices 28, 'Not'
+go
+insert_exam_choices 28, '<='
+go
+insert_exam_choices 29, 'Polymorphism'
+go
+insert_exam_choices 29, 'Abstraction'
+go
+insert_exam_choices 29, 'Encapsulation'
+go
+insert_exam_choices 29, 'Recursion'
+go
+insert_exam_choices 30, '+'
+go
+insert_exam_choices 30, '+='
+go
+insert_exam_choices 30, '&'
+go
+insert_exam_choices 30, '||'
+go
+insert_exam_choices 31, 'True'
+go
+insert_exam_choices 31, 'False'
+go
+insert_exam_choices 32, 'True'
+go
+insert_exam_choices 32, 'False'
+go
+insert_exam_choices 33, 'True'
+go
+insert_exam_choices 33, 'False'
+go
+insert_exam_choices 34, 'True'
+go
+insert_exam_choices 34, 'False'
+go
+insert_exam_choices 35, 'True'
+go
+insert_exam_choices 35, 'False'
+go
+insert_exam_choices 36, 'True'
+go
+insert_exam_choices 36, 'False'
+go
+insert_exam_choices 37, 'True'
+go
+insert_exam_choices 37, 'False'
+go
+insert_exam_choices 38, 'True'
+go
+insert_exam_choices 38, 'False'
+go
+insert_exam_choices 39, 'True'
+go
+insert_exam_choices 39, 'False'
+go
+insert_exam_choices 40, 'True'
+go
+insert_exam_choices 40, 'False'
